ef0bce05283d4cc95ab184810c3b658cbca3b9c1
Tyelk <66346393+Tyelk@users.noreply.github.com>
2026-08-09 19:31:03 +1200

    Created movement contract
    - Created contract_movement to show public method signatures
    - Created movement subpackage to help group movement related files
    - Created direction and tile_effect enums
    - Created tile_data to make it simpler to transfer data to the UI
    - Created fake_game_mode to help simulate real game modes

---
+++ src/zimp/domain/common/contract_movement.py
@@ -0,0 +1,47 @@
+from typing import runtime_checkable, Protocol
+
+from zimp.domain.movement.direction import Direction
+from zimp.domain.common.error_code import ErrorCode
+from zimp.domain.common.result import Result
+from zimp.domain.movement.tile_data import TileData
+from zimp.domain.movement.tile_effect import TileEffect
+from zimp.support.fake_game_mode import GameMode
+
+
+@runtime_checkable
+class MovementContract(Protocol):
+    """Contract for movement"""
+
+    def move_player(self, mode: GameMode, direction: Direction) -> Result:
+        """Move player in given direction"""
+
+    def rotate_placement_tile(self, mode: GameMode) -> ErrorCode | None:
+        """Rotate the placement tile"""
+
+    def lock_placement_tile(self, mode: GameMode) -> ErrorCode | None:
+        """Lock the rotation of the placement tile"""
+
+    def need_zombie_door(self, mode: GameMode) -> bool:
+        """Check if map needs a zombie door"""
+
+    def get_tile_data(self) -> list[TileData]:
+        """Get data for displayed tiles"""
+
+    def get_player_position(self) -> tuple[int, int]:
+        """Gets players position"""
+
+    def get_zombie_count(self) -> int:
+        """Get zombie count on current tile"""
+
+    def defeat_zombies(self) -> None:
+        """Defeat zombies on current tile"""
+
+    def add_zombies(self, number_of_zombies: int) -> None:
+        """Add zombies to current tile"""
+
+    def get_tile_effect(self) -> TileEffect:
+        """Get tile effect of current tile"""
+
+    def reset(self, starting_position: tuple[int, int] | None = None,
+              randomizer_seed: int | None = None) -> ErrorCode | None:
+        """Reset tiles and optionally set player starting position and/or tile randomizer seed"""
---
+++ src/zimp/domain/movement/tile_data.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+from zimp.domain.movement.direction import Direction
+
+
+@dataclass(frozen=True)
+class TileData:
+    id: int
+    position: tuple[int, int]
+    rotation: int
+    zombie_door: Direction | None
+    zombie_count: int
+    is_locked: bool
